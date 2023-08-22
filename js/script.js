f75093f812c1809df971388eeb8a464942d971c8
Emam Elmorsy <137660003+emamelmorsy55@users.noreply.github.com>
2023-08-22 13:17:20 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){ 
+    $('[data-toggle="popover"]').alert("");
+    $('[data-toggle="popover"]').popover();
+   
+  });
+  document.getElementById("pops").onclick = function(){alert("You Win Best Player") };
+
+ 
